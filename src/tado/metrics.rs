6bae47dc6c0a86ee68adb6ff4d857adff5893b29
Vincent Composieux <vincent@composieux.fr>
2021-10-03 12:10:13 +0200

    Allow HOT_WATER device types

--- src/tado/metrics.rs
+++ src/tado/metrics.rs
@@ -11,72 +11,80 @@ lazy_static! {
     pub static ref ACTIVITY_HEATING_POWER: GaugeVec = register_gauge_vec!(
         "tado_activity_heating_power_percentage",
         "The % of heating power in a specific zone.",
-        &["zone"]
+        &["zone", "type"]
     ).unwrap();
 
     pub static ref ACTIVITY_AC_POWER: GaugeVec = register_gauge_vec!(
         "tado_activity_ac_power_value",
         "The value of ac power in a specific zone.",
-        &["zone"]
+        &["zone", "type"]
     ).unwrap();
 
     pub static ref SETTING_TEMPERATURE: GaugeVec = register_gauge_vec!(
         "tado_setting_temperature_value",
         "The temperature of a specific zone in celsius degres.",
-        &["zone", "unit"]
+        &["zone", "type", "unit"]
     ).unwrap();
 
     pub static ref SENSOR_TEMPERATURE: GaugeVec = register_gauge_vec!(
         "tado_sensor_temperature_value",
         "The temperature of a specific zone in celsius degres.",
-        &["zone", "unit"]
+        &["zone", "type", "unit"]
     ).unwrap();
 
     pub static ref SENSOR_HUMIDITY_PERCENTAGE: GaugeVec = register_gauge_vec!(
         "tado_sensor_humidity_percentage",
         "The % of humidity in a specific zone.",
-        &["zone"]
+        &["zone", "type"]
     ).unwrap();
 }
 
 pub fn set(zones: Vec<ZoneStateResponse>) {
     for zone in zones {
+        let device_type: String = zone.state_response.setting.deviceType;
+
         // The setting temperature may be null in the API response, if the
         // zone's heating mode is turned off. If the temperature setting is
         // absent, from the API response we'll simply not set its gauge values.
         if let Some(setting_temperature) = zone.state_response.setting.temperature {
             // setting temperature
             let value: f64 = setting_temperature.celsius;
-            SETTING_TEMPERATURE.with_label_values(&[zone.name.as_str(), "celsius"]).set(value);
-            info!("-> {} -> setting temperature (celsius): {}", zone.name, value);
+            SETTING_TEMPERATURE.with_label_values(&[zone.name.as_str(), device_type.as_str(), "celsius"]).set(value);
+            info!("-> {} ({}) -> setting temperature (celsius): {}", zone.name, device_type.as_str(), value);
 
             let value: f64 = setting_temperature.fahrenheit;
-            SETTING_TEMPERATURE.with_label_values(&[zone.name.as_str(), "fahrenheit"]).set(value);
-            info!("-> {} -> setting temperature (fahrenheit): {}", zone.name, value);
+            SETTING_TEMPERATURE.with_label_values(&[zone.name.as_str(), device_type.as_str(), "fahrenheit"]).set(value);
+            info!("-> {} ({}) -> setting temperature (fahrenheit): {}", zone.name, device_type.as_str(), value);
         } else {
-            info!("-> {} -> setting temperature (celsius): Off", zone.name);
-            info!("-> {} -> setting temperature (fahrenheit): Off", zone.name);
+            info!("-> {} ({}) -> setting temperature (celsius): Off", zone.name, device_type.as_str());
+            info!("-> {} ({}) -> setting temperature (fahrenheit): Off", zone.name, device_type.as_str());
         }
 
         // sensor temperature
-        let value: f64 = zone.state_response.sensorDataPoints.insideTemperature.celsius;
-        SENSOR_TEMPERATURE.with_label_values(&[zone.name.as_str(), "celsius"]).set(value);
-        info!("-> {} -> sensor temperature (celsius): {}", zone.name, value);
+        if let Some(inside_temperature) = zone.state_response.sensorDataPoints.insideTemperature {
+            // celsius
+            let value: f64 = inside_temperature.celsius;
+            SENSOR_TEMPERATURE.with_label_values(&[zone.name.as_str(), device_type.as_str(), "celsius"]).set(value);
+            info!("-> {} ({}) -> sensor temperature (celsius): {}", zone.name, device_type.as_str(), value);
 
-        let value: f64 = zone.state_response.sensorDataPoints.insideTemperature.fahrenheit;
-        SENSOR_TEMPERATURE.with_label_values(&[zone.name.as_str(), "fahrenheit"]).set(value);
-        info!("-> {} -> sensor temperature (fahrenheit): {}", zone.name, value);
+            // fahrenheit
+            let value: f64 = inside_temperature.fahrenheit;
+            SENSOR_TEMPERATURE.with_label_values(&[zone.name.as_str(), device_type.as_str(), "fahrenheit"]).set(value);
+            info!("-> {} ({}) -> sensor temperature (fahrenheit): {}", zone.name, device_type.as_str(), value);
+        }
 
         // sensor humidity
-        let value: f64 = zone.state_response.sensorDataPoints.humidity.percentage;
-        SENSOR_HUMIDITY_PERCENTAGE.with_label_values(&[zone.name.as_str()]).set(value);
-        info!("-> {} -> sensor humidity: {}%", zone.name, value);
+        if let Some(humidity) = zone.state_response.sensorDataPoints.humidity {
+            let value: f64 = humidity.percentage;
+            SENSOR_HUMIDITY_PERCENTAGE.with_label_values(&[zone.name.as_str(), device_type.as_str()]).set(value);
+            info!("-> {} ({}) -> sensor humidity: {}%", zone.name, device_type.as_str(), value);
+        }
 
         // heating power
         if let Some(heating_power) = zone.state_response.activityDataPoints.heatingPower {
             let value: f64 = heating_power.percentage;
-            ACTIVITY_HEATING_POWER.with_label_values(&[zone.name.as_str()]).set(value);
-            info!("-> {} -> heating power: {}%", zone.name, value);
+            ACTIVITY_HEATING_POWER.with_label_values(&[zone.name.as_str(), device_type.as_str()]).set(value);
+            info!("-> {} ({}) -> heating power: {}%", zone.name, device_type.as_str(), value);
         }
 
         // ac power
@@ -87,8 +95,8 @@ pub fn set(zones: Vec<ZoneStateResponse>) {
                 _     => 0.0,
             };
 
-            ACTIVITY_AC_POWER.with_label_values(&[zone.name.as_str()]).set(value);
-            info!("-> {} -> ac power: {}", zone.name, value);
+            ACTIVITY_AC_POWER.with_label_values(&[zone.name.as_str(), device_type.as_str()]).set(value);
+            info!("-> {} ({}) -> ac power: {}", zone.name, device_type.as_str(), value);
         }
     }
 }
